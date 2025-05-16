e2225637721dcf2e12f6eeba48ff2e1763e32a09
Oleg Mozharovskyi <oleg.mozharovskyi-ext@jci.com>
2025-05-17 01:19:39 +0200

    [JLP]: add operation waiter and zone with GPU lister

--- python/create_llm_vm.py
+++ python/create_llm_vm.py
@@ -1,31 +1,33 @@
 from googleapiclient import discovery
 import paramiko
 import time
-from oauth2client.client import GoogleCredentials
+from google.oauth2 import service_account
+from jb_llm_logger import logger
+from google.cloud import compute_v1
 
-PROJECT = "your-gcp-project-id"
+PROJECT = "jb-llm-plugin"
 ZONE = "europe-west4-a"
 INSTANCE_NAME = "ollama-python-vm"
 
 def create_vm_with_gpu():
-    credentials = GoogleCredentials.get_application_default()
+    credentials = service_account.Credentials.from_service_account_file("sa-keys/jb-llm-plugin-sa.json")
     compute = discovery.build('compute', 'v1', credentials=credentials)
     config = {
         'name': INSTANCE_NAME,
-        'machineType': f"zones/{ZONE}/machineTypes/n1-highmem-4",
+        'machineType': f"zones/{ZONE}/machineTypes/n1-standard-4",
         'disks': [{
             'boot': True,
             'autoDelete': True,
             'initializeParams': {
                 'sourceImage': 'projects/ubuntu-os-cloud/global/images/family/ubuntu-2204-lts',
-                'diskSizeGb': '50'
+                'diskSizeGb': '30'
             }
         }],
         'networkInterfaces': [{
             'network': 'global/networks/default',
             'accessConfigs': [{'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}]
         }],
-        'scheduling': {'onHostMaintenance': 'TERMINATE', 'automaticRestart': True},
+        'scheduling': {'onHostMaintenance': 'TERMINATE', 'automaticRestart': False},
         'guestAccelerators': [{
             'acceleratorType': f'zones/{ZONE}/acceleratorTypes/nvidia-tesla-t4',
             'acceleratorCount': 1
@@ -40,7 +42,8 @@ def create_vm_with_gpu():
         zone=ZONE,
         body=config
     ).execute()
-    print(f"Instance creation started: {operation['name']}")
+    logger.info(f"Instance creation started: {operation['name']}")
+    wait_for_operation(compute,PROJECT,ZONE,operation['name'])
 
 def run_ssh_commands(host_ip, username='ubuntu'):
     commands = [
@@ -100,7 +103,9 @@ def get_instance_external_ip(project_id, zone, instance_name):
         return None
 
 def wait_for_instance_running(project_id, zone, instance_name, timeout=300, interval=10):
-    credentials = GoogleCredentials.get_application_default()
+    credentials = service_account.Credentials.from_service_account_file(
+        "sa-keys/jb-llm-plugin-sa.json"
+    )
     compute = discovery.build('compute', 'v1', credentials=credentials)
 
     elapsed = 0
@@ -122,3 +127,42 @@ def wait_for_instance_running(project_id, zone, instance_name, timeout=300, inte
 
     print("Timeout waiting for instance to become RUNNING.")
     return False
+
+def list_zones_with_gpus(project_id, gpu_name):
+    credentials = service_account.Credentials.from_service_account_file("sa-keys/jb-llm-plugin-sa.json")
+    client = compute_v1.AcceleratorTypesClient(credentials=credentials)
+    request = compute_v1.AggregatedListAcceleratorTypesRequest(project=project_id)
+    response = client.aggregated_list(request=request)
+
+    zones_with_gpu = set()
+    for zone, scoped_list in response:
+        if scoped_list.accelerator_types:
+            for acc in scoped_list.accelerator_types:
+                if acc.name == gpu_name:
+                    zones_with_gpu.add(zone.split('/')[-1])
+    return zones_with_gpu
+
+
+
+def wait_for_operation(compute, project, zone, operation_name, timeout=300):
+    logger.info(f"Waiting for '{operation_name}' to complete with timeout in '{timeout}' seconds...")
+    start_time = time.time()
+    while True:
+        result = compute.zoneOperations().get(
+            project=project,
+            zone=zone,
+            operation=operation_name
+        ).execute()
+        if result['status'] == 'DONE':
+            if 'error' in result:
+                raise Exception(f"There was error upon completion: {result['error']}")
+            logger.info("Done successfully.")
+            return
+        if time.time() - start_time > timeout:
+            raise TimeoutError(f"After timeout of '{timeout}' seconds operation is still runnung.")
+        time.sleep(1)
+
+if __name__ == "__main__":
+    # create_vm_with_gpu()
+    gpu_list = list_zones_with_gpus(PROJECT, 'nvidia-tesla-t4')
+    logger.info(gpu_list)
