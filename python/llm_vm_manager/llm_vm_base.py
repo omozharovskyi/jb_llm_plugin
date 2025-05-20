3c5ab63ce1992d28d1a2311be2c981b320f53553
Oleg Mozharovskyi <oleg.mozharovskyi-ext@jci.com>
2025-05-20 17:50:21 +0200

    [JLP-0028]: add get_my_ip to abstract class

--- python/llm_vm_manager/llm_vm_base.py
+++ python/llm_vm_manager/llm_vm_base.py
@@ -1,8 +1,16 @@
 from abc import ABC, abstractmethod
+import requests
+from llm_vm_manager.config import ConfigLoader
+from llm_vm_manager.jb_llm_logger import logger
+from llm_vm_manager.ssh_base import SSHManager
+from llm_vm_manager.ssh_client import SSHClient
+
 
 class LLMVirtualMachineManager(ABC):
-    def __init__(self, project_id: str):
-        self.project_id = project_id
+    def __init__(self, configuration: ConfigLoader):
+        self.llm_vm_manager_config = configuration
+        self.ssh_manager = SSHManager()
+        self.ssh_client = SSHClient()
 
     @abstractmethod
     def create_instance(self, name: str):
@@ -23,3 +31,10 @@ class LLMVirtualMachineManager(ABC):
     @abstractmethod
     def list_instances(self):
         pass
+
+    def get_my_ip(self):
+        logger.debug("Getting my IP address...")
+        my_ip_url = self.llm_vm_manager_config.get("my_ip_url")
+        my_ip = requests.get(my_ip_url).text
+        logger.debug(my_ip)
+        return my_ip
