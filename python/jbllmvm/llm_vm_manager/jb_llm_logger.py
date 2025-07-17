785dbfd282ec235650d853ec8f2768d4669ac5a5
Oleg Mozharovskyi <oleg.mozharovskyi-ext@jci.com>
2025-07-17 15:44:10 +0200

    [JLP-0034]: LLM-GPU description

--- python/jbllmvm/llm_vm_manager/jb_llm_logger.py
+++ python/jbllmvm/llm_vm_manager/jb_llm_logger.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - JBLLMVM - %(levelname)s - %(message)s",
+    format="%(asctime)s JBLLMVM:%(levelname)s: %(message)s",
     handlers=[ logging.StreamHandler() ]
 )
 
